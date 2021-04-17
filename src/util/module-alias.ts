e8ec7c53379b5d265ad9a11e8bf660d5c30d3802
Jean Carlos <jeanjnx@gmail.com>
2021-04-17 13:31:05 -0300

    setup

---
+++ src/util/module-alias.ts
@@ -0,0 +1,15 @@
+import  * as path from 'path';
+import moduleAlias from 'module-alias';
+
+const files = path.resolve(__dirname, '../..');
+
+/**
+ * Even set path alias in tsconfig.ts
+ * it's necessary implement this, else
+ * on compilation we'll get errors because the app
+ * won't find the paths
+ */
+moduleAlias.addAliases({
+    '@src': path.join(files, 'src'),
+    '@test': path.join(files, 'test')
+})
