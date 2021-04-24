9f4a956e3418f910b3f2bcf6dc66c4018e41be22
Jean Carlos <jeanjnx@gmail.com>
2021-04-24 18:39:46 -0300

    trata erros de users

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import './util/module-alias'; //should be imported before the uses of aliases
 import * as database from '@src/database';
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
     constructor(private port = 3000) {
@@ -24,7 +25,12 @@ export class SetupServer extends Server {
     private setupControllers(): void {
         const forecastController = new ForecastController();
         const beachesController = new BeachesController();
-        this.addControllers([forecastController, beachesController]);
+        const usersController = new UsersController();
+        this.addControllers([
+            forecastController,
+            beachesController,
+            usersController,
+        ]);
     }
 
     private async databaseSetup(): Promise<void> {
